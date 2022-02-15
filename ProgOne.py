53ccf354b834439d95a4e43737381272adead8bd
thrandale <64854392+thrandale@users.noreply.github.com>
2022-02-14 17:00:05 -0700

    create input function

--- ProgOne.py
+++ ProgOne.py
@@ -5,6 +5,9 @@ Program Make Change
 Gets the user's wallet amount, the cost of their purchase, and prints the amount of change they will receive.
 '''
 
+from pickle import FALSE
+
+
 DOLLAR = 1
 QUARTER = .25
 DIME = .10
@@ -21,32 +24,45 @@ def validateInput(input, max):
     try:
         input = float(input)
     except ValueError:
-        print("Input must be a number")
-        exit()
+        print("Input must be a number\n")
+        return False
 
     # Checks if the input is a positive number and less than the max
     if input <= 0:
-        print("Input must be greater than 0")
-        exit()
+        print("Input must be greater than 0\n")
+        return False
     elif input > max:
-        print("Input must be less than or equal to $", max)
-        exit()
+        print("Input must be less than or equal to $\n", max)
+        return False
     else:
-        return round(input, 2)
+        return True
+
+
+def getInput(prompt, inputMax):
+    '''
+    Gets the user's input and validates it.
+    '''
+    attempts = 3
+    while attempts > 0:
+        userInput = input(prompt)
+        if validateInput(userInput, inputMax):
+            return round(float(userInput), 2)
+        attempts -= 1
+
+    print("Maximum attempts reached. Exiting program.\n")
+    exit()
 
 
 # get the amount of money in the wallet
-walletAmount = input("How much money is in your wallet? ")
-walletAmount = validateInput(walletAmount, 1000000)
+walletAmount = getInput("How much money do you have in your wallet? ", 1000000)
 
 # get the amount of money to spend
-costOfItem = input("How much money do you want to spend? ")
-costOfItem = validateInput(costOfItem, walletAmount)
+costOfItem = getInput("How much does the item cost? ", walletAmount)
 
 # calculate the change
 change = walletAmount - costOfItem
 
-print("Your change is: $" + str(round(change, 2)))
+print("\nYour change is: $" + str(round(change, 2)))
 print("Dollars:", str(int(change / DOLLAR)))
 
 change %= DOLLAR
